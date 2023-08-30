41ba4f0ed59efe565ab15b5ea9da3a9b42b6c320
skullface <skullface@users.noreply.github.com>
2023-08-30 16:00:12 -0400

    restructure empty bottle

--- script/venues/empty-bottle.py
+++ script/venues/empty-bottle.py
@@ -19,40 +19,40 @@ shows = calendar.find_all('div', class_='eb-item')
 all_shows_list = []
 
 for show in shows:
-  all_shows_data = {} 
-  sold_out = ...
-  artists_elements = show.find('ul', class_='performing').find_all('li')
   artists_list = []
+  sold_out = False
+  artists_elements = show.find('ul', class_='performing').find_all('li')
+  
   for artist in artists_elements:
     if '*SOLD OUT*' in artist.text.strip():
       sold_out = True
+    
+    artist_text = artist.text.strip()
+    if any(keyword in artist_text for keyword in ['DAY PASS', 'Empty Bottle Yoga', 'Hard Country Honky Tonk', 'Fantasy Brunch']):
+      continue
+    artists_list.append(artist_text.replace('*SOLD OUT* ', '').replace(' - ', ': '))
+
+  if artists_list:
+    all_shows_data = {}
+
+    if sold_out:
+      all_shows_data['sold_out'] = True
+
+    link = show.find('a', class_='buy-button')
+    all_shows_data['link'] = link.get('href')
+
+    date = show.find('div', class_='date')
+    date = date.text.strip().replace('Mon ', '').replace('Tue ', '').replace('Wed ', '').replace('Thu ', '').replace('Fri ', '').replace('Sat ', '').replace('Sun ', '')
+    date = date.replace('August ', '2023-08-').replace('September ', '2023-09-').replace('October ', '2023-10-').replace('November ', '2023-11-').replace('December ', '2023-12-').replace('January ', '2024-01-').replace('February ', '2024-02-').replace('March ', '2024-03-').replace('April ', '2024-04-').replace('May ', '2024-05-').replace('June ', '2024-06-').replace('July ', '2024-07-') + 'T20:00:00'
+    all_shows_data['date'] = date.replace('-1T', '-01T').replace('-2T', '-02T').replace('-3T', '-03T').replace('-4T', '-04T').replace('-5T', '-05T').replace('-6T', '-06T').replace('-7T', '-07T').replace('-8T', '-08T').replace('-9T', '-09T')
+
+    venue = show.find('a', class_='event-venue')
+    if venue:
+      all_shows_data['venue'] = venue.text.strip().replace('At: ', '')
     else:
-      sold_out = False
-    if 'DAY PASS' in artist.text.strip():
-      ...
-    elif "Empty Bottle Yoga" in artist.text.strip():
-      ...
-    elif 'Hard Country Honky Tonk' in artist.text.strip():
-      ...
-    elif 'Fantasy Brunch' in artist.text.strip():
-      ...
-    else:
-      all_shows_data['artist'] = artists_list
-    artists_list.append(artist.text.strip().replace('*SOLD OUT* ', '').replace(' - ', ': '))
-  if sold_out:
-    all_shows_data['sold_out'] = True
-  link = show.find('a', class_='buy-button')
-  all_shows_data['link'] = link.get('href')
-  date = show.find('div', class_='date')
-  date = date.text.strip().replace('Mon ', '').replace('Tue ', '').replace('Wed ', '').replace('Thu ', '').replace('Fri ', '').replace('Sat ', '').replace('Sun ', '')
-  date = date.replace('August ', '2023-08-').replace('September ', '2023-09-').replace('October ', '2023-10-').replace('November ', '2023-11-').replace('December ', '2023-12-').replace('January ', '2024-01-').replace('February ', '2024-02-').replace('March ', '2024-03-').replace('April ', '2024-04-').replace('May ', '2024-05-').replace('June ', '2024-06-').replace('July ', '2024-07-') + 'T20:00:00'
-  all_shows_data['date'] = date.replace('-1T', '-01T').replace('-2T', '-02T').replace('-3T', '-03T').replace('-4T', '-04T').replace('-5T', '-05T').replace('-6T', '-06T').replace('-7T', '-07T').replace('-8T', '-08T').replace('-9T', '-09T')
-  venue = show.find('a', class_='event-venue')
-  if venue:
-    all_shows_data['venue'] = venue.text.strip().replace('At: ', '')
-  else:
-    all_shows_data['venue'] = 'Empty Bottle'
-  all_shows_list.append(all_shows_data)
+      all_shows_data['venue'] = 'Empty Bottle'
+
+    all_shows_list.append(all_shows_data)
 
 all_shows_json = json.dumps(all_shows_list, indent=2) 
 print(all_shows_json)
