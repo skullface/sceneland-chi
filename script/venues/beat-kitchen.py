3576b9c416dacb5e02ccb0fd21dd4ed27b57eed7
skullface <skullface@users.noreply.github.com>
2023-08-30 18:54:01 -0400

    ignore shows before today's date

--- script/venues/beat-kitchen.py
+++ script/venues/beat-kitchen.py
@@ -15,6 +15,7 @@ def should_skip_artist(artist_text):
   return any(keyword in artist_text for keyword in excluded_keywords)
 
 all_shows_list = []
+current_date = datetime.now()
 
 for show in shows:
   all_shows_data = {} 
@@ -32,12 +33,14 @@ for show in shows:
     all_shows_data['sold_out'] = True
 
   date = show.find('span', class_='tw-event-date')
-  date = date.text.strip().replace(', 2023', '').replace(', 2024', '').replace('August ', '2023-08-').replace('September ', '2023-09-').replace('October ', '2023-10-').replace('November ', '2023-11-').replace('December ', '2023-12-').replace('January ', '2024-01-').replace('February ', '2024-02-').replace('March ', '2024-03-').replace('April ', '2024-04-').replace('May ', '2024-05-').replace('June ', '2024-06-').replace('July ', '2024-07-') + 'T20:00:00'
-  all_shows_data['date'] = date.replace('-1T', '-01T').replace('-2T', '-02T').replace('-3T', '-03T').replace('-4T', '-04T').replace('-5T', '-05T').replace('-6T', '-06T').replace('-7T', '-07T').replace('-8T', '-08T').replace('-9T', '-09T')
+  date = datetime.strptime(date.text.strip(), '%B %d, %Y')
+  if date < current_date:
+    continue
+  all_shows_data['date'] = str(date).replace(' 00:00:00', 'T20:00:00')
 
   all_shows_data['venue'] = 'Beat Kitchen'
 
   all_shows_list.append(all_shows_data)
 
-all_shows_json = json.dumps(all_shows_list, indent=2) 
+all_shows_json = json.dumps(all_shows_list, indent=2, default=str) 
 print(all_shows_json)
